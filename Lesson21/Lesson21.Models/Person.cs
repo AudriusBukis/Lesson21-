eb3fe6a2213db9f9e7dffe74571d18d8ac74b2b6
AudriusBukis <audriusbukis@gmail.com>
2022-02-21 21:17:01 +0200

    program upload

---
+++ Lesson21/Lesson21.Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson21.Models
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public List<Pets> Pets { get; set; }
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+            Pets = new List<Pets>();
+        }
+    }
+}
